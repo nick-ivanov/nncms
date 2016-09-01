7ab32074808cb97b2917cfbb858c1577d34c9148
Nick Ivanov <nnrowan@gmail.com>
2016-09-01 16:53:33 -0500

    Development version 0.0.13 See CHANGELOG.txt for more information

---
+++ data_and_settings/v1/settings.php
@@ -0,0 +1,17 @@
+<?php
+$title = "SM230 Demonstration Website";
+$footer_text = "Copyright &copy; 2016 John Appleseed";
+
+$pages = array(
+    'home' => 'Home',
+    'news' => 'News',
+    'about' => 'About'
+);
+
+$menu = array(
+    array('Home', 'index.php'),
+    array('About', 'index.php?about'),
+    array('News', 'index.php?news')
+);
+
+?>
