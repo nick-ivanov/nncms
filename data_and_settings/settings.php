f52cccd8e441dce0fb03abb4ce3b47f1fd9c56f6
Nick Ivanov <nnrowan@gmail.com>
2016-09-06 17:36:06 -0500

    Development version 0.1.2 See CHANGELOG.txt for more information

---
+++ data_and_settings/settings.php
@@ -0,0 +1,22 @@
+<?php
+$title = "nnBits: the fun side of programming";
+$footer_text = "Copyright &copy; 2016 Nick Ivanov<br/>
+Text and visual content of this website, except when otherwise is specified, is licensed under a <br/>
+<a rel=\"license\" href=\"http://creativecommons.org/licenses/by/4.0/\">Creative Commons Attribution 4.0 International License</a>.";
+
+$pages = array(
+    'home' => 'Home',
+    'about' => 'About',
+    'projects' => 'Projects',
+    'fcposl' => 'Fair Credit Permissive Open Source License',
+    'kernel' => 'Linux Kernel Development Pages'
+);
+
+$menu = array(
+    array('Home', 'index.php'),
+    array('About', 'index.php?about'),
+    array('Projects', 'index.php?projects'),
+    array('Linux Kernel Development', 'index.php?kernel')
+);
+
+?>
