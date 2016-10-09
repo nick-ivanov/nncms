4f03f8e8d24d15f114f658f11c4bb46be4db92d6
Nick Ivanov <nick@nnbits.org>
2016-10-08 18:51:39 -0600

    Sat Oct  8 18:51:39 MDT 2016

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    SM230: pretty secure CMS
+    nnBits CMS: pretty secure CMS
     Copyright (C) 2016 Nick Ivanov <nick@nnbits.org>
 
     This program is free software: you can redistribute it and/or modify
@@ -17,6 +17,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+  You can use any name for data and settings. If you do not want website
+  users browse it (one reason for this could be the desire not to disclose
+  unpublished pages and data), just deny access to it in .htaccess.
+*/
 $data_and_settings_dir = "data_and_settings";
 
 require "$data_and_settings_dir/settings.php";
@@ -29,6 +34,13 @@ require "src/get_header.php";
 require "src/get_go.php";
 require "src/get_title.php";
 
+/* 
+   When a user types, for example, http://example.com?ARGUMENT,
+   the argument is scrutinized by the security and page existense
+   check. As a result, $go is an adequate and safe response to an
+   argument requested in the URL (e.g., existing page ID,
+   Error 404 page ID, etc.)
+*/ 
 $go = get_go($pages, $home_page);
 
 $body = get_body_skeleton(
